14cb0be4d69c910497843fef1d6431ae0a49fc90
Phillip Marshall <wizpig64@gmail.com>
2021-05-06 20:03:19 -0700

    added EntoMap model from my main project

--- app/models.py
+++ app/models.py
@@ -1,3 +1,95 @@
-from django.db import models
+import os.path
+import re
+import tempfile
+from base64 import b64decode
 
-# Create your models here.
+from django.core.files.base import ContentFile
+from django.core.files.images import ImageFile
+from django.core.files.storage import default_storage
+from django.db import models
+from PIL import Image
+
+MAP_SIZE = (230, 210)  # width, height
+
+
+def report_map_background_path(instance, filename):
+    return f"ento/report-maps/{instance.pk}bg{os.path.splitext(filename)[1]}"
+
+
+def report_map_sketch_path(instance, filename):
+    return f"ento/report-maps/{instance.pk}sk{os.path.splitext(filename)[1]}"
+
+
+def report_map_composite_path(instance, filename):
+    return f"ento/report-maps/{instance.pk}{os.path.splitext(filename)[1]}"
+
+
+class EntoMap(models.Model):
+    field_id = models.CharField(max_length=20)
+    map_background = models.ImageField(
+        upload_to=report_map_background_path,
+        verbose_name="map background",
+    )
+    map_sketch = models.ImageField(
+        upload_to=report_map_sketch_path,
+        verbose_name="map sketch",
+    )
+    map_composite = models.ImageField(
+        upload_to=report_map_composite_path,
+        height_field="map_height",
+        width_field="map_width",
+        verbose_name="map composite",
+        help_text="The map background and sketch are composited together into this while saving.",
+    )
+    map_height = models.PositiveSmallIntegerField(null=True, default=MAP_SIZE[1])
+    map_width = models.PositiveSmallIntegerField(null=True, default=MAP_SIZE[0])
+
+    class Meta:
+        verbose_name = "field map"
+        verbose_name_plural = "field maps"
+
+    def get_background(self):
+        """Returns a file object holding the default areamap for this field."""
+        for name in [
+            f"ento/field-maps/{self.field_id}.png",
+            "ento/field-maps/general.png",
+        ]:
+            try:
+                return ImageFile(
+                    default_storage.open(name),
+                    # name=os.path.basename(name),  # needed with 2.2.21+
+                )
+            except FileNotFoundError:
+                pass
+
+    def set_sketch_datauri(self, uri):
+        """Creates a ContentFile object from a base64-encoded datauri.
+
+        For example: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUA
+            AAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO
+            9TXL0Y4OHwAAAABJRU5ErkJggg=='
+
+        """
+        data = re.compile(r"base64,(.*)").search(uri).group(1)
+        self.map_sketch = ContentFile(b64decode(data), name="sketch.png")
+
+    def compile_maps(self):
+        if self.map_background and self.map_sketch:
+            # Flatten together into map_composite
+            composite = Image.new("RGBA", MAP_SIZE, (255, 255, 255, 255))
+            background = Image.open(self.map_background).convert("RGBA")
+            sketch = Image.open(self.map_sketch).convert("RGBA")
+            composite.paste(background, mask=background)
+            composite.paste(sketch, mask=sketch)
+            # Save to a temp file
+            composite_file = tempfile.NamedTemporaryFile(suffix=".png", delete=False)
+            composite.save(composite_file, format="png")
+            composite_file.close()
+            # Save to the field's storage
+            self.map_composite = ImageFile(
+                open(composite_file.name, "rb"),
+                # name=os.path.basename(composite_file.name),  # needed with 2.2.21+
+            )
+            # (this temp file gets removed with a post-save signal)
+            self._trash_later = composite_file.name
+            print("compile done")
--- app/views.py
+++ app/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from base64 import b64encode
 
-# Create your views here.
+from django.core.files.storage import default_storage
+from django.http import HttpResponse
+
+from app.models import EntoMap
+
+
+def view(request):
+    # this view should create 6 files in storage/ento/report-maps/
+
+    with default_storage.open("user_sketch.png") as sketch:
+        sketch_uri = f"data:image/png;base64,{b64encode(sketch.read()).decode()}"
+
+    print("creating and saving map for field a")
+    fieldmap_a = EntoMap.objects.create(field_id="field_a")
+    fieldmap_a.map_background = fieldmap_a.get_background()
+    fieldmap_a.set_sketch_datauri(sketch_uri)
+    fieldmap_a.compile_maps()
+    fieldmap_a.save()
+
+    print("creating and saving map for other")
+    fieldmap_b = EntoMap.objects.create(field_id="other")  # use general bg
+    fieldmap_b.map_background = fieldmap_b.get_background()
+    fieldmap_b.set_sketch_datauri(sketch_uri)
+    fieldmap_b.compile_maps()
+    fieldmap_b.save()
+
+    return HttpResponse("i did it!")
